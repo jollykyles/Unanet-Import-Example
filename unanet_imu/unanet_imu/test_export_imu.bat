aca5f4622eb8ed903628f8f8d6574ccfd7d202f2
Kyle Ortiz <37933909+jollykyles@users.noreply.github.com>
2025-10-02 11:39:03 -0400

    Delete unanet_imu/unanet_imu directory

--- unanet_imu/unanet_imu/test_export_imu.bat
+++
@@ -1,3 +0,0 @@
-REM script to run ant and execute a test
-set ant_home=bin/ant
-bin/ant/bin/ant.bat -f imu_export_build.xml test
--- unanet_imu/unanet_imu/unanet_imu.bat
+++
@@ -1,3 +0,0 @@
-REM script to run ant and execute the unanet_imu process
-set ant_home=bin/ant
-bin/ant/bin/ant.bat -Dlog4j2.formatMsgNoLookups="true" -f imu_build.xml import
